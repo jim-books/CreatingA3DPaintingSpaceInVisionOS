e17169e54a330ab4478b648645c945f9e1f208ff
jimbookshkust <khhungac@connect.ust.hk>
2025-08-31 16:08:08 +0800

    implemented update logic using RealityKit engine

--- Painting/Components/ClosureComponent.swift
+++ Painting/Components/ClosureComponent.swift
@@ -5,6 +5,33 @@ Abstract:
 A system to update an entity's transform in real time.
 */
 
+import SwiftUI
+import RealityKit
+
+struct ClosureComponent: Component {
+    // TimeInterval is a type alias for Double type
+    let closure: (TimeInterval) -> Void
+    
+    init(closure: @escaping (TimeInterval) -> Void) {
+        self.closure = closure
+        ClosureSystem.registerSystem()
+    }
+}
+
+struct ClosureSystem: System {
+    static let query = EntityQuery(where: .has(ClosureComponent.self))
+    
+    init(scene: RealityKit.Scene) {}
+    
+    func update(context: SceneUpdateContext){
+        for entity in context.entities (matching: Self.query, updatingSystemWhen: .rendering) {
+            guard let comp = entity.components[ClosureComponent.self] else { continue }
+            comp.closure(context.deltaTime)
+        }
+    }
+}
+
+
 //import SwiftUI
 //import RealityKit
 //
