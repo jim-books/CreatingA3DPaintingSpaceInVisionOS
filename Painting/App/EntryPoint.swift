b783ccbb8ed9cf59bbc108f923ec9bd00a01e606
jimbookshkust <khhungac@connect.ust.hk>
2025-08-31 15:42:07 +0800

    comment out entry point sample

--- Painting/App/EntryPoint.swift
+++ Painting/App/EntryPoint.swift
@@ -5,18 +5,18 @@ Abstract:
 The app's entry point.
 */
 
-import SwiftUI
-
-@main
-struct EntryPoint: App {
-    var body: some Scene {
-        WindowGroup {
-            MainView()
-        }
-
-        // The immersive space that the app defines as a part of the scene.
-        ImmersiveSpace(id: "PaintingScene") {
-            PaintingView()
-        }
-    }
-}
+//import SwiftUI
+//
+//@main
+//struct EntryPoint: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            MainView()
+//        }
+//
+//        // The immersive space that the app defines as a part of the scene.
+//        ImmersiveSpace(id: "PaintingScene") {
+//            PaintingView()
+//        }
+//    }
+//}
